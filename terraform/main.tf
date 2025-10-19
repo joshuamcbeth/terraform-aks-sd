a4e4db7534366827dde9cb571fe38b2ac551e5bf
Joshua McBeth <joshua.mcbeth@gmail.com>
2025-10-19 18:13:34 -0400

    Assign cluster principal Contributor for cluster RG so it can access public IPs

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,3 +95,9 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   role_definition_name = "AcrPull"
   scope          = azurerm_container_registry.acr.id
 }
+
+resource "azurerm_role_assignment" "aks_lb_contributor" {
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  role_definition_name = "Contributor"
+  scope                = azurerm_resource_group.rg.id
+}
