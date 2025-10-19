d06978c2b02d1c325e5c98f7da0121f260243764
Joshua McBeth <joshua.mcbeth@gmail.com>
2025-10-19 15:52:15 -0400

    Remove unnecessary subnet delegation

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,16 +41,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "aks-delegation"
-    service_delegation {
-      name = "Microsoft.ContainerService/managedClusters"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action"
-      ]
-    }
-  }
 }
 
 # AKS cluster
